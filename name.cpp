6c483e22c05cf6555b7f3b986c9afbd7b81d1c85
Jogeza <55192247+Jogeza@users.noreply.github.com>
2020-03-01 16:00:15 +0300

    c plus plus work

--- name.cpp
+++
@@ -1,9 +0,0 @@
-//aprogramme which prints atleast two friends
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"Solomon";
-	cout<<"WALUGEMBE";
-	return 0;
-}
--- university.cpp
+++
@@ -1,9 +0,0 @@
-//a programme which prints names of schools
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"UGANDA\t\t\tMARTYRS";
-	cout<<"MAKERERE";
-	return 0;
-}
